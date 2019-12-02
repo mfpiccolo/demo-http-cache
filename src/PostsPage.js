d14b425af4dd7efe7c897f7dbfd9a2fdeb9c7541
Michael Piccolo <>
2019-12-01 20:27:58 -0800

    prefetch

--- src/PostsPage.js
+++ src/PostsPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { fetchPosts, useFetchPostsSubscription } from './api'
+import { fetchPosts, fetchPost, useFetchPostsSubscription } from './api'
 import Loader from './Loader'
-import { Link } from 'react-router-dom'
+import PreFetchHoverLink from './PreFetchHoverLink'
 
 export default function PostsPage() {
   const [posts, setPosts] = useState([])
@@ -19,12 +19,18 @@ export default function PostsPage() {
       <h2>Posts</h2>
       <div style={{ backgroundColor: '#00688B', width: '30%', margin: 'auto' }}>
         <Loader subscription={useFetchPostsSubscription} />
-        <button onClick={() => fetchAndSetPosts()}>Refresh</button>
+        <button onMouseDown={() => fetchAndSetPosts()}>Refresh</button>
       </div>
 
       {posts.map(({ id, title }) => (
         <div key={id}>
-          <Link to={`/posts/${id}`}>{title}</Link>
+          <PreFetchHoverLink
+            to={`/posts/${id}`}
+            onHover={() => fetchPost(id)}
+            delay={65}
+          >
+            {title}
+          </PreFetchHoverLink>
         </div>
       ))}
     </div>
---
+++ src/PreFetchHoverLink.js
@@ -0,0 +1,16 @@
+import React, { Children } from 'react'
+import useHover from 'react-use-hover'
+import { Link } from 'react-router-dom'
+
+export default function PreFetchHoverLink({ to, onHover, delay, children }) {
+  const [isHovering, hoverProps] = useHover({ mouseEnterDelayMS: delay })
+
+  if (isHovering) {
+    onHover()
+  }
+  return (
+    <Link {...hoverProps} to={to}>
+      {children}
+    </Link>
+  )
+}
